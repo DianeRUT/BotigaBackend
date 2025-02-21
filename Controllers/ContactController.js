89ac8a44e2fde1d54deba78e225700aaa94527a5
DianeRUT <ingabirerdianna250@gmail.com>
2025-02-21 12:57:35 +0200

    new commit

--- Controllers/ContactController.js
+++ Controllers/ContactController.js
@@ -40,4 +40,54 @@ export const getContactById = async (req, res) => {
     } catch (error) {
       res.status(500).json({ success: false, message: "Server error", error: error.message });
     }
+  }
+
+  export const deleteContactById = async (req, res) => {
+    try {
+      
+      const id = req.params.id; 
+  
+      if (!id) {
+        return res.status(400).json({ success: false, message: "Invalid ID" });
+      }
+  
+      const deletedContact = await Contact.findByIdAndDelete(id);
+  
+      if (!deletedContact) {
+        return res.status(404).json({ success: false, message: "Contact not found" });
+      }
+  
+      res.status(200).json({ success: true, message: "Contact deleted successfully" });
+    } catch (error) {
+
+      console.error("Error deleting contact:", error); 
+      res.status(500).json({ success: false, message: "Server error", error: error.message });
+    }
+  }
+
+  export const updateContactById = async (req, res) => {
+    try {
+      console.log("Received request params:", req.params); 
+      console.log("Received request body:", req.body); 
+  
+      const { id } = req.params; 
+      const updatedData = req.body; 
+  
+      if (!id) {
+        return res.status(400).json({ success: false, message: "Invalid ID" });
+      }
+  
+      const updatedContact = await Contact.findByIdAndUpdate(id, updatedData, { new: true, runValidators: true });
+  
+      if (!updatedContact) {
+        return res.status(404).json({ success: false, message: "Contact not found" });
+      }
+  
+      res.status(200).json({ success: true, message: "Contact updated successfully", contact: updatedContact });
+    } catch (error) {
+      console.error("Error updating contact:", error); 
+      res.status(500).json({ success: false, message: "Server error", error: error.message });
+    }
   };
+  
+  
--- routes/ContactPath.js
+++ routes/ContactPath.js
@@ -1,8 +1,12 @@
-import { createContact, getAllContact, getContactById } from "../Controllers/ContactController.js";
+import { createContact, getAllContact, getContactById, deleteContactById, updateContactById } from "../Controllers/ContactController.js";
 import express from "express";
 
 const contactRouter=express.Router();
 contactRouter.post("/createContact", createContact)
 contactRouter.get("/getAllContact", getAllContact)
 contactRouter.get("/:id", getContactById); 
+contactRouter.get("/:id", getContactById); 
+contactRouter.delete("/:id", deleteContactById); 
+contactRouter.update("/:id", updateContactById); 
+
 export default contactRouter;
