d232c01c2d12e778a107dbace99cdfe13c8439dd
DianeRUT <ingabirerdianna250@gmail.com>
2025-02-28 15:26:55 +0200

    new commit

--- Middleware/roleIdentification.js
+++ Middleware/roleIdentification.js
@@ -1,3 +1,5 @@
+import User from "../models/userModal.js";
+
 export const admin= (req, res, next) =>{
 if (req.user.userRole !=="admin"){
     return res.status(403).json({message:"Access denied, please contact admin"})
